205b31ceda6079d11ebdfdc4f570160b66d42ea2
Juana Solano <juana.solano@correo.tdea.edu.co>
2023-10-08 19:40:21 -0500

    Add: Components and config Firebase

---
+++ src/credentials.tsx
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDpcuWoFxKleUT_7nfrnh4f_YWGI_XaEa0',
+  authDomain: 'restaurantjuana-18908.firebaseapp.com',
+  projectId: 'restaurantjuana-18908',
+  storageBucket: 'restaurantjuana-18908.appspot.com',
+  messagingSenderId: '793005470666',
+  appId: '1:793005470666:web:418329be94990c8d5e9533',
+  measurementId: 'G-CJ8RSY7JZ5'
+}
+
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig)
+export default appFirebase
+ 

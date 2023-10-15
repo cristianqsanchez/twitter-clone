1068a14fad3aa250c2a0eb3312eae0a695623793
Juana Solano <juana.solano@correo.tdea.edu.co>
2023-10-15 11:12:40 -0500

    Add: Routes followers and following

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import SignIn from './components/SignIn'
 import './index.css'
+import Followers from './components/Followers'
+import Following from './components/Following'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -11,6 +13,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/followers" element={<Followers />} />
+        <Route path="/following" element={<Following />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>

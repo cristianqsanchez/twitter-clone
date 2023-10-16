1cb26b3fe0bd1ef1f2402c83e22f3e8d9ce4cdd0
Juana Solano <juana.solano@correo.tdea.edu.co>
2023-10-16 10:02:54 -0500

    Fix: status follow

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,6 +7,7 @@ function Home() {
   const navigate = useNavigate()
   const [list, setList] = useState([])
   const [following, setFollowing] = useState(false)
+  const [followingUsers, setFollowingUsers] = useState([])
   const [followersCount, setFollowersCount] = useState(0)
   const [followingCount, setFollowingCount] = useState(0)
   const location = useLocation()
@@ -43,6 +44,7 @@ function Home() {
         const userData = userSnapshot.data()
         if (userData && userData.following) {
           setFollowingCount(userData.following.length)
+          setFollowingUsers(userData.following)
         }
       } catch (error) {
         console.log(error)
@@ -58,20 +60,15 @@ function Home() {
       }
     }
     getList()
-  }, [list])
+  }, [id])
 
   const followUser = async (userIdToFollow) => {
     const users = doc(db, 'users', id)
-
-    if (following) {
-      await updateDoc(users, {
-        following: arrayRemove(userIdToFollow)
-      })
-    } else {
-      await updateDoc(users, {
-        following: arrayUnion(userIdToFollow)
-      })
-    }
+    console.log(id)
+    console.log(userIdToFollow)
+    await updateDoc(users, {
+      following: arrayUnion(userIdToFollow)
+    })
   }
 
   return (
@@ -85,20 +82,17 @@ function Home() {
             <div className="hidden w-full md:block md:w-auto" id="navbar-default">
               <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
-                  <a href="/home" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-                </li>
-                <button
-                  className='"text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-                  onClick={() => navigate('/')}>Sign Out
-                </button>
-                <li>
+                  <button
+                    className='"text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                    onClick={() => navigate('/')}>Sign Out
+                  </button>
                 </li>
               </ul>
             </div>
           </div>
         </nav>
 
-        <div className="flex flex-col items-right justify-left px-6 py-8 mx-auto md:h-screen">
+        <div className="flex flex-col items-right justify-left px-6 py-8">
           <h1 className='dark:text-white flex flex-col  items-center' >Welcome {fullname}</h1>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-4 space-y-4 md:space-y-6 sm:p-8">
@@ -106,12 +100,12 @@ function Home() {
                 <p className='text-slate-50'>Followers</p>
                 <a
                   className='className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" '
-                  onClick={() => navigate('/followers', { state: { id: idUser } })}
+                  onClick={() => navigate('/followers', { state: { id: idUser, fullname: fullname } })}
                 >{followersCount}</a>
                 <p className='text-slate-50'>Following</p>
                 <a
                   className='className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" '
-                  onClick={() => navigate('/following', { state: { id: idUser } })}
+                  onClick={() => navigate('/following', { state: { id: idUser, fullname: fullname } })}
                 >{followingCount}</a>
               </div>
             </div>
@@ -132,7 +126,7 @@ function Home() {
                           <a href="/home"
                             className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
                             onClick={() => followUser(list.id)}
-                          >{following ? 'Unfollow' : 'Follow'}</a>
+                          >{followingUsers.includes(list.id) ? 'Unfollow' : 'Follow'}</a>
                         </td>
                       </tr>
                     </tbody>

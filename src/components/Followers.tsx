b873c32b2ab820aad9434c21731f7385caf5fee1
Juana Solano <juana.solano@correo.tdea.edu.co>
2023-10-16 00:28:35 -0500

    Fix: Modify nav

--- src/components/Followers.tsx
+++ src/components/Followers.tsx
@@ -3,26 +3,28 @@ import { useNavigate, useLocation } from 'react-router-dom'
 import appFirebase, { db } from '@config/firebase'
 import { collection, getDoc, getDocs, query, doc, addDoc, onSnapshot, getFirestore, setDoc, where, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore'
 
-function Followers () {
-    const navigate = useNavigate()
-    return (
+function Followers() {
+  const location = useLocation()
+  const { state } = location
+  const [list, setList] = useState([])
+  const navigate = useNavigate()
+  const { fullname, id } = state
+  const idUser = id
+  return (
         <>
             <div className="bg-gray-50 dark:bg-gray-900 mx-auto md:h-screen">
                 <nav className="bg-white border-gray-200 dark:bg-gray-900">
                     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                        <a href="/home" className="flex items-center">
+                        <a onClick={() => navigate('/home', { state: { id: idUser, fullname: fullname } })} className="flex items-center">
                             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Twitter</span>
                         </a>
                         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                                 <li>
-                                    <a href="/home" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-                                </li>
-                                <button
-                                    className='"text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-                                    onClick={() => navigate('/')}>Sign Out
-                                </button>
-                                <li>
+                                    <button
+                                        className='"text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                                        onClick={() => navigate('/')}>Sign Out
+                                    </button>
                                 </li>
                             </ul>
                         </div>
@@ -62,6 +64,6 @@ function Followers () {
             </div>
 
         </>
-    )
+  )
 }
 export default Followers
--- src/components/Following.tsx
+++ src/components/Following.tsx
@@ -8,23 +8,23 @@ function Following() {
   const { state } = location
   const [list, setList] = useState([])
   const navigate = useNavigate()
-  const { id } = state
+  const { fullname, id } = state
   const idUser = id
   useEffect(() => {
     const getList = async () => {
       try {
-         // Paso 1: Consulta el arreglo 'following' para el usuario autenticado
-         const userDocRef = doc(db, 'users', idUser)
-         const userDoc = await getDoc(userDocRef)
+        // Paso 1: Consulta el arreglo 'following' para el usuario autenticado
+        const userDocRef = doc(db, 'users', idUser)
+        const userDoc = await getDoc(userDocRef)
 
-         if (userDoc.exists()) {
-           const userFollowingArray = userDoc.data().following
+        if (userDoc.exists()) {
+          const userFollowingArray = userDoc.data().following
 
-           // Establece el arreglo 'following' en el estado
-           setList(userFollowingArray || [])
-         } else {
-           console.log('Usuario no encontrado.')
-         }
+          // Establece el arreglo 'following' en el estado
+          setList(userFollowingArray || [])
+        } else {
+          console.log('Usuario no encontrado.')
+        }
       } catch (error) {
         console.log(error)
       }
@@ -37,19 +37,16 @@ function Following() {
             <div className="bg-gray-50 dark:bg-gray-900 mx-auto md:h-screen">
                 <nav className="bg-white border-gray-200 dark:bg-gray-900">
                     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                        <a onClick={() => navigate('/home')} className="flex items-center">
+                        <a onClick={() => navigate('/home', { state: { id: idUser, fullname: fullname } })} className="flex items-center">
                             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Twitter</span>
                         </a>
                         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                                 <li>
-                                    <a href="/home" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-                                </li>
                                 <button
                                     className='"text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
                                     onClick={() => navigate('/')}>Sign Out
                                 </button>
-                                <li>
                                 </li>
                             </ul>
                         </div>

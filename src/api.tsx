70b7e56a2e492731fdadca05062630d17bb90dce
Cristian Quintero Sánchez <cristian.quintero@pragma.com.co>
2023-10-19 21:05:58 -0500

    remove unused api handler

--- src/api.tsx
+++
@@ -1,6 +0,0 @@
-import { db } from '@config/firebase'
-import { collection, getDoc, getDocs, query, doc, addDoc, deleteDoc, updateDoc } from 'firebase/firestore'
-
-function saveUser () {
-  addDoc(collection(db, 'users'), { name })
-}

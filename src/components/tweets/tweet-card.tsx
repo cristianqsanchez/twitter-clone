e55ea14e2a29859e66f5e2b3530330134abae232
Cristian Quintero Sánchez <cristian.quintero@pragma.com.co>
2023-10-21 02:08:45 -0500

    add tweet component

---
+++ src/components/tweets/tweet-card.tsx
@@ -0,0 +1,18 @@
+import { TweetData } from './tweets.types'
+import { Link } from 'react-router-dom'
+import { getTimeAgo } from '@utils/relative-time'
+
+export default function Tweet ({ content, date, author, username }: TweetData) {
+  return (
+    <article className='flex-col bg-gray-950 text-white m-2 p-4 rounded-2xl'>
+      <header className='pb-2'>
+        <Link className='font-bold' to={`/user/${username}`}>{author} </Link>
+        <span className='opacity-30'>@{username} </span>
+        <time className='opacity-30'>· {getTimeAgo(date)}</time>
+      </header>
+      <main>
+        <p>{content}</p>
+      </main>
+    </article>
+  )
+}

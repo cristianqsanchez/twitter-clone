77670fe5241c97ae88a97f0147ea6d4d94741f14
Cristian Quintero Sánchez <cristian.quintero@pragma.com.co>
2023-10-21 07:27:07 -0500

    remove legacy firebase auth context provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import { FirebaseAuthProvider } from './context'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <FirebaseAuthProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </FirebaseAuthProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )

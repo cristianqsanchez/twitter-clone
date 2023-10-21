730ad4f959493585396d9935c018c14e64e9e7f1
Cristian Quintero Sánchez <cristian.quintero@pragma.com.co>
2023-10-21 07:01:06 -0500

    fix: remove console.log

--- src/components/tweets/list-of-tweets.tsx
+++ src/components/tweets/list-of-tweets.tsx
@@ -3,7 +3,6 @@ import { TweetData } from './tweets.types'
 
 export default function ListOfTweets ({ myTweets, followedUserTweets }: { myTweets: TweetData[], followedUserTweets: TweetData[] }) {
   const tweets = myTweets.concat(followedUserTweets.flat(1))
-  console.log(tweets)
   return (
     <main className='grid place-content-center'>
         <ul className='max-w-prose'>

dd9470108b07214834e7966c7a1f1caf2538628d
Cristian Quintero Sánchez <cristian.quintero@pragma.com.co>
2023-10-21 08:54:00 -0500

    feat: add sort by date in tweets

--- src/components/tweets/list-of-tweets.tsx
+++ src/components/tweets/list-of-tweets.tsx
@@ -1,12 +1,13 @@
 import Tweet from './tweet-card'
 import { TweetData } from './tweets.types'
 
-export default function ListOfTweets ({ myTweets, followedUserTweets }: { myTweets: TweetData[], followedUserTweets: TweetData[] }) {
+export default function ListOfTweets ({ myTweets, followedUserTweets = [] }: { myTweets: TweetData[], followedUserTweets: TweetData[] }) {
   const tweets = myTweets.concat(followedUserTweets.flat(1))
+  const sortedTweets = tweets.sort((a, b) => b.date - a.date)
   return (
     <main className='grid place-content-center'>
         <ul className='max-w-prose'>
-          {tweets?.map((tweet, id) => (
+          {sortedTweets?.map((tweet, id) => (
             <li key={id}>
               <Tweet {...tweet} />
             </li>
